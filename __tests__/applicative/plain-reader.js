fc1241c865a1727fcc7a01671f1c19aad4220341
Max Gherman <gheran.maxim@gmail.com>
2018-08-05 22:57:18 +1000

    all applicative laws

--- __tests__/applicative/plain-reader.js
+++ __tests__/applicative/plain-reader.js
@@ -1,4 +1,5 @@
-import { identity } from 'ramda';
+import { identity, compose, flip, partial } from 'ramda';
+import { dot } from '@common/utils';
 import { applicative as appBase } from '@control/applicative/plain-reader';
 
 const applicative = appBase();
@@ -69,7 +70,7 @@ describe('Reader Applicative',() => {
 
     describe('Applicative first law (Identity): pure id <*> v = v', () => {
         const arg1 = applicative.pure(identity);
-        const arg2 = (x) => x + 3;
+        const arg2 = (r) => r + 3;
         const result = applicative.lift(arg1, arg2);
 
         expect(result(10)).toBe(arg2(10));
@@ -103,5 +104,40 @@ describe('Reader Applicative',() => {
 
         expect(result1(10)).toBe(result2(10));
         expect(result1(10)).toBe(8);
+    });
+
+    describe('Application forth law (Composition): pure (.) <*> u <*> v <*> w = u <*> (v <*> w)', () => {
+        const u = (r) => (x) => x + 2;
+        const v = (r) => (x) => x * 3;
+        const arg1 = applicative.pure(dot);
+        
+        it('plain', () => {
+            const w = (r) => r + 3;
+        
+            const result1 = applicative.lift(applicative.lift(applicative.lift(arg1, u), v), w);
+            const result2 = applicative.lift(u, applicative.lift(v, w));
+
+            expect(result1(1)).toBe(result2(1));
+            expect(result1(1)).toBe(14);
+        });
+       
+        it('with compose', () => {
+            const w = (r) => r + 6;
+            const lift = applicative.lift;
+          
+            const result1 = compose(
+                flip(lift)(w),
+                flip(lift)(v),
+                partial(lift, [arg1])
+            )(u);
+
+            const result2 = compose(
+                partial(lift, [u]),
+                partial(lift, [v])
+            )(w); 
+
+            expect(result1(1)).toBe(result2(1));
+            expect(result1(1)).toBe(23);
+        });
     });
 });
