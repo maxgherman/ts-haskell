7007784e331fd76dab258c7770f49b9c3216ad53
maxgherman <german.maxim@gmail.com>
2025-08-18 11:02:26 +1000

    test: cover comonad base derivations

--- test/ghc/base/comonad.test.ts
+++ test/ghc/base/comonad.test.ts
@@ -1,6 +1,8 @@
 import tap from 'tap'
 import { comonad as writerComonad } from 'control/writer/comonad'
+import { functor as writerFunctor } from 'control/writer/functor'
 import { writer, WriterBox } from 'control/writer/writer'
+import { comonad as createComonad } from 'ghc/base/comonad'
 import { tuple2 } from 'ghc/base/tuple/tuple'
 
 const cm = writerComonad<string>()
@@ -20,6 +22,41 @@ tap.test('Comonad', async (t) => {
         const f = (w: WriterBox<string, number>) => w.runWriter()[0] + 1
         const left = cm.extend(f, wa)
         const right = cm['<$>'](f, cm.duplicate(wa))
+        t.same(run(left), run(right))
+    })
+
+    t.test('derives duplicate from extend', async (t) => {
+        const functor = writerFunctor<string>()
+        const base = {
+            extract: <A>(wb: WriterBox<string, A>): A => wb.runWriter()[0],
+            extend: <A, B>(f: (wb: WriterBox<string, A>) => B, wb: WriterBox<string, A>): WriterBox<string, B> =>
+                writer(() => {
+                    const [, w] = wb.runWriter()
+                    return tuple2(f(wb), w)
+                }),
+        }
+        const derived = createComonad(base as any, functor as any) as any
+
+        const dup = derived.duplicate(wa)
+        const ext = derived.extend((w: any) => w, wa)
+        t.same(run(dup), run(ext))
+    })
+
+    t.test('derives extend from duplicate', async (t) => {
+        const functor = writerFunctor<string>()
+        const base = {
+            extract: <A>(wb: WriterBox<string, A>): A => wb.runWriter()[0],
+            duplicate: <A>(wb: WriterBox<string, A>): WriterBox<string, WriterBox<string, A>> =>
+                writer(() => {
+                    const [, w] = wb.runWriter()
+                    return tuple2(wb, w)
+                }),
+        }
+        const derived = createComonad(base as any, functor as any) as any
+
+        const f = (w: WriterBox<string, number>) => w.runWriter()[0] + 1
+        const left = derived.extend(f, wa)
+        const right = derived['<$>'](f, derived.duplicate(wa))
         t.same(run(left), run(right))
     })
 })
